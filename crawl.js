60d96cbbe46ec9fa58f525adab6f32baad2df8b5
Nick <nick@smplfi.com>
2024-08-06 10:19:13 -0400

    added recursive crawling logic, basic reporting, report sorting and tests

--- crawl.js
+++ crawl.js
@@ -19,6 +19,49 @@ function getUrlsFromHtml(htmlBody, baseUrl) {
     return formatted
 }
 
+async function crawlPage(baseURL, currentURL, pages = []) {
+    try {
+	
+	if (!currentURL.includes(normalizeURL(baseURL))) {
+	    console.log(`baseURL: ${baseURL}, currentURL: ${currentURL}`)
+	    return pages
+	}
+	const fixedURL = normalizeURL(currentURL)
+	if (fixedURL in pages) {
+	    pages[fixedURL]++
+	} else {
+	    pages[fixedURL] = 1
+	}
+	const parsedPage = await parsePage(currentURL)
+	const foundURLs = getUrlsFromHtml(parsedPage, baseURL)
+	for (let i = 0; i < foundURLs.length; i++) {
+	    const rec = await crawlPage(baseURL, foundURLs[i], pages)
+	    pages.push(...rec)
+	}
+	return pages
 
-export { normalizeURL, getUrlsFromHtml };
+    } catch (err) {
+	console.log(err.message)
+    }
+}
+
+async function parsePage(url) {
+    try {
+        const resp = await fetch(url)
+        if (resp.status >= 400) {
+	    throw new Error(`fetch returned error ${resp.status}`)
+	}
+	const ct = resp.headers.get('content-type')
+	if (!ct || !ct.includes('text/html')) {
+	    throw new Error(`data returned not not appropriate type, expecting text/html, got ${ct}`)
+	}
+	return await resp.text()
+    } catch (err) {
+	console.log(`Error: ${err.message} occurred parsing page at: ${url}. Continuing`)
+	return ""
+    }
+}
+
+export { normalizeURL, getUrlsFromHtml, crawlPage };
+
 
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+import { argv } from 'node:process';
+import { crawlPage } from './crawl.js';
+import { printReport } from './report.js';
+
+async function main() {
+    if (argv.length !== 3) {
+	throw new Error("Incorrect Argument Count should only pass one argument")
+    }
+    console.log(`Crawler starting at: ${argv[2]}`)
+    const pages = await crawlPage(argv[2], argv[2])
+    printReport(pages)
+}
+
+await main()
